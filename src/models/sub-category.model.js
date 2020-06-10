f22accfcb65c71fcbe42830e98f5b1df5dad0ab7
QuocLamHong <quoclamhong1312@gmail.com>
2020-06-10 21:21:14 +0700

    Add tag and sub category model, fix post to writer add post

---
+++ src/models/sub-category.model.js
@@ -0,0 +1,46 @@
+const db = require('../utils/db');
+
+const TBL = 'SUB_CATEGORY';
+const CAT_TBL = 'CATEGORY';
+
+module.exports = {
+    loadAll: async () => {
+        const rows = await db.load(`SELECT * FROM ${TBL} WHERE isDeleted = 0`);
+
+        if(rows.length === 0) {
+            return null;
+        }
+
+        return rows;
+    },
+    findByName: async (name) => {
+        const rows = await db.load(`SELECT * FROM ${TBL} WHERE name = '${name}' AND isDeleted = 0`);
+
+        if(rows.length === 0) {
+            return null;
+        }
+
+        return rows[0];
+    },
+    findByID: async (id) => {
+        const rows = await db.load(`SELECT * FROM ${TBL} WHERE id = ${id} AND isDeleted = 0`);
+
+        if(rows.length === 0) {
+            return null;
+        }
+
+        return rows[0];
+    },
+    loadByParentCategory: async (parentName) => {
+        const rows = await db.load(`SELECT * FROM ${TBL} SUB_CAT JOIN ${CAT_TBL} CAT ON SUB_CAT.category_id = CAT.id WHERE CAT.name = '${parentName}' AND SUB_CAT.isDeleted = 0`);
+
+        if(rows.length === 0) {
+            return null;
+        }
+
+        return rows;
+    },
+    add: (subCatObj) => {
+        return db.create(TBL, subCatObj);
+    }
+}
--- src/routes/writer.route.js
+++ src/routes/writer.route.js
@@ -4,13 +4,22 @@ const config = require('../configs/default')
 const storage = multer.diskStorage(config.multerImagePost)
 const upload = multer({storage:storage})
 
-
 router.get('/add/post', (req, res) => {
-    res.render('writer/add-post')
+    res.render('writer/add-post');
+})
+
+router.post('/add/post', upload.single('featured_image'), (req, res) => {
+    try {
+        req.body.featured_image = req.file.filename;
+        console.log(req.body);
+    } catch(e) {
+        console.log(e);
+        res.redirect('/');
+    }
 })
 
 router.post('/image', upload.single('file'), (req, res) => {
-    res.json({ location: `/public/images/post/${req.file.filename}` })
+    res.json({ location: `/public/images/post/${req.file.filename}` });
 })
 
 module.exports = router;
