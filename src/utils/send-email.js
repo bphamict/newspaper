7183ad8e71285b6eeed0b1b19419b9f1727a4cd3
Bao Pham <bao.pt@appfast.io>
2020-06-11 11:38:42 +0700

    get code from templateData

--- src/utils/send-email.js
+++ src/utils/send-email.js
@@ -12,9 +12,9 @@ const mailgun = require('mailgun-js')({
 const sendEmail = ({ to, subject, type, templateData }) => {
   let url = '';
   if (type === authentication.typeOfCode.CONFIRM_ACCOUNT) {
-    url = `https://${app.domain}/auth/confirm-account?code=${code}`;
+    url = `https://${app.domain}/auth/confirm-account?code=${templateData.code}`;
   } else if (type === authentication.typeOfCode.RESET_PASSWORD) {
-    url = `https://${app.domain}/auth/reset-password?code=${code}`;
+    url = `https://${app.domain}/auth/reset-password?code=${templateData.code}`;
   }
   const data = {
     from: `${app.name} <noreply@${app.domain}>`,
