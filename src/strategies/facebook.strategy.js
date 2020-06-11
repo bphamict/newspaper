e6a0e637bdb5986869bfb4e42cb8020e1a704f8c
Bao Pham <bao.pt@appfast.io>
2020-06-11 12:19:38 +0700

    update callback url

--- src/strategies/facebook.strategy.js
+++ src/strategies/facebook.strategy.js
@@ -7,7 +7,7 @@ passport.use(
     {
       clientID: process.env.FACEBOOK_APP_ID,
       clientSecret: process.env.FACEBOOK_APP_SECRET,
-      callbackURL: '/auth/facebook/callback',
+      callbackURL: `https://${app.domain}/auth/facebook/callback`,
       enableProof: true,
       profileFields: ['id', 'displayName', 'photos', 'email'],
     },
