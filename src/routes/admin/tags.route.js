177bb33fddd6ef528f0d8969b03b86cf1a2c2f32
TrTrongHieu <truongtronghieu2004@gmail.com>
2020-07-03 12:48:12 +0700

    update slug tags admin

--- src/routes/admin/tags.route.js
+++ src/routes/admin/tags.route.js
@@ -3,50 +3,59 @@ const db = require('../../utils/db');
 const tagsModel = require('../../models/admin/tags.model');
 const router = express.Router();
 const isAdmin = require('../../middlewares/isAdmin.middleware');
+const slugify = require('slugify');
 
 router.get('/', isAdmin, async function (req, res) {
-    const list = await tagsModel.all();
-    res.render('Admin/Tags/list', {tags: list, empty: list.length === 0});
+  const list = await tagsModel.all();
+  res.render('Admin/Tags/list', { tags: list, empty: list.length === 0 });
 });
 
 router.get('/add', isAdmin, function (req, res) {
-    res.render('Admin/Tags/add');
+  res.render('Admin/Tags/add');
 });
 
 router.post('/add', async function (req, res) {
-    await tagsModel.add(req.body);
-    res.render('Admin/Tags/add');
+  const entity = req.body;
+  entity.slug = slugify(req.body.name, {
+    remove: /[*+~.()'"!=:@|^&${}[\]`;/?,\\<>%]/g,
+    lower: true,
+    locale: 'vi',
+  });
+  await tagsModel.add(entity);
+  res.render('Admin/Tags/add');
 });
 
-router.get('/edit', isAdmin, async function (req, res) {
-    const id = +req.query.id || -1;
-    const row = await tagsModel.single(id);
-    if(row.length === 0){
-        res.send('Invalid parameter.');
-    }
-    const tag = row[0];
-    res.render('Admin/Tags/edit', {tag});
+router.get('/:slug/edit', isAdmin, async function (req, res) {
+  const slug = req.params.slug;
+  const row = await tagsModel.singleBySlug(slug);
+  if (row.length === 0) {
+    res.send('Invalid parameter.');
+  }
+  const tag = row[0];
+  res.render('Admin/Tags/edit', { tag });
 });
 
 router.post('/update', async function (req, res) {
-    const entity = {
-        id: req.body.id,
-        name: req.body.name,
-        isDeleted: req.body.isDeleted
-    }
-    await tagsModel.update(entity);
-    res.redirect('/Admin/Tags');
+  const entity = req.body;
+  entity.slug = slugify(req.body.name, {
+    remove: /[*+~.()'"!=:@|^&${}[\]`;/?,\\<>%]/g,
+    lower: true,
+    locale: 'vi',
+  });
+  await tagsModel.update(entity);
+  res.redirect('/Admin/Tags');
 });
 
 router.post('/delete', async function (req, res) {
-    const entity = {
-        id: req.body.id,
-        name: req.body.name,
-        isDeleted: 1
-    }
-    await tagsModel.del(entity);
-    res.redirect('/Admin/Tags');
+  const entity = req.body;
+  entity.isDeleted = 1;
+  entity.slug = slugify(req.body.name, {
+    remove: /[*+~.()'"!=:@|^&${}[\]`;/?,\\<>%]/g,
+    lower: true,
+    locale: 'vi',
+  });
+  await tagsModel.del(entity);
+  res.redirect('/Admin/Tags');
 });
 
 module.exports = router;
-
