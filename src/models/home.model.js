26b5bc3d6dab7ebb65cae8c6975ca0a96a03ce5a
dinhhientuan <hien.tuan1601@gmail.com>
2020-07-19 02:35:19 +0700

    add home's category with subCats breadcrumbs

--- src/models/home.model.js
+++ src/models/home.model.js
@@ -1,7 +1,11 @@
 const db = require('../utils/db');
 
 module.exports = {
-    all:function(){
+    top3View:function(){
         return db.load(`SELECT featured_image as Image, title as Title FROM post ORDER BY view_count DESC LIMIT 3`);
+    },
+
+    catWithSubs:function(){
+        return db.load(`SELECT category.name as catName, sub_category.name as subCatName FROM category join sub_category on category.id = sub_category.category_id`);
     },
 };
--- src/routes/home.route.js
+++ src/routes/home.route.js
@@ -2,11 +2,11 @@ const router = require('express').Router();
 const homeModel = require('../models/home.model');
 
 router.get('/', async function(req, res){
-    const list = await homeModel.all();
+    const postsForCarousel = await homeModel.top3View();
 
-    const first = list[0];
+    const first = postsForCarousel[0];
     const rest = [];
-    list.forEach(element => {
+    postsForCarousel.forEach(element => {
         if(element === first){
             return;
         }
@@ -16,9 +16,39 @@ router.get('/', async function(req, res){
         })
     });
 
+    const catsWithSubs = await homeModel.catWithSubs();
+
+    name = catsWithSubs[0].catName;
+    subs = [];
+    cats = [];
+    catsWithSubs.forEach(element => {
+        if(element.catName === name)
+        {
+            subs.push(element.subCatName);
+        }
+        else
+        {
+            cats.push({
+                catName: name,
+                subCatName: subs,
+            })
+
+            name = element.catName;
+            subs = [];
+            subs.push(element.subCatName);
+        }
+    })
+    cats.push({
+        catName: name,
+        subCatName: subs,
+    })
+
+    console.log(cats);
+
     res.render('home',{
         first: first,
-        post: rest      
+        post: rest,
+        cats: cats,  
     });
 })
 
