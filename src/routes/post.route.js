2b1e8ca259280e9fef4590cd14929b3928dfea0d
dinhhientuan <hien.tuan1601@gmail.com>
2020-07-28 21:32:54 +0700

    add post detail's category,tag href

--- src/routes/post.route.js
+++ src/routes/post.route.js
@@ -15,7 +15,7 @@ router.get('/error', (req, res) => {
 router.get('/:slug', async (req, res) => {
     const slug = req.params.slug;
     const post = await postModel.loadBySlugWithCategoryAndSubCategoryName(slug);
-    
+
     if(!post) {
         res.redirect('/post/error');
     }
